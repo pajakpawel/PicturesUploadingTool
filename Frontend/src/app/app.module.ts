da0157fed970c87d2bcd78c4ea9a6a5906007029
pajakpawel <pajakpawel@vp.pl>
2021-01-18 20:36:46 +0100

    Generated components for navigation purposes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { PicturesListComponent } from './pictures-list/pictures-list.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    PicturesListComponent,
+    UploadPictureComponent
   ],
   imports: [
     BrowserModule,
