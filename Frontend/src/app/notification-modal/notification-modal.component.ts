69313bf888d51e4ab379434a3c10fde3c036016b
pajakpawel <pajakpawel@vp.pl>
2021-01-21 00:00:01 +0100

    Implemented frontend notification modal component to observe its service state

--- Frontend/src/app/notification-modal/notification-modal.component.ts
+++ Frontend/src/app/notification-modal/notification-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { NotificationModalManagementService } from '../services/notification-modal-management/notification-modal-management.service';
 
 @Component({
   selector: 'app-notification-modal',
@@ -6,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-modal.component.css']
 })
 export class NotificationModalComponent implements OnInit {
+  displayNotificationModal: Observable<boolean> = of(false);
+  notificationMessage: Observable<string> = of('');
 
-  constructor() {
+  constructor(private notificationModalManagement: NotificationModalManagementService) {
   }
 
   ngOnInit(): void {
+    this.displayNotificationModal = this.notificationModalManagement.observeNotificationModalDisplay();
+    this.notificationMessage = this.notificationModalManagement.observeNotificationMessage();
+  }
+
+  closeNotificationModal(): void {
+    this.notificationModalManagement.closeNotificationModal();
   }
 
 }
