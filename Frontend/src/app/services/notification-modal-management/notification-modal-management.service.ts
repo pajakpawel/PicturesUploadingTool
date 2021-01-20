e1337d054743c93dd9d67ab7c9a04ac4a4413b1e
pajakpawel <pajakpawel@vp.pl>
2021-01-20 23:57:21 +0100

    Implemented frontend notification modal management service

--- Frontend/src/app/services/notification-modal-management/notification-modal-management.service.ts
+++ Frontend/src/app/services/notification-modal-management/notification-modal-management.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationModalManagementService {
+  private displayNotificationModal = new BehaviorSubject(false);
+  private notificationMessage = new BehaviorSubject('Blank');
 
   constructor() {
+  }
+
+  observeNotificationModalDisplay(): Observable<boolean> {
+    return this.displayNotificationModal.asObservable();
+  }
+
+  observeNotificationMessage(): Observable<string> {
+    return this.notificationMessage.asObservable();
+  }
+
+  showNotificationModal(message: string): void {
+    this.notificationMessage.next(message);
+    this.displayNotificationModal.next(true);
+  }
+
+  closeNotificationModal(): void {
+    this.notificationMessage.next('');
+    this.displayNotificationModal.next(false);
   }
 }
