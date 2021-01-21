c9599bc04b7ff16806499fe7641b2e21481159e5
pajakpawel <pajakpawel@vp.pl>
2021-01-21 21:55:11 +0100

    Added to frontend constants an api URL for deleting specified picture from server

--- Frontend/src/app/constants.ts
+++ Frontend/src/app/constants.ts
@@ -3,4 +3,5 @@ export class AppConstants {
   public static get apiURL(): string { return 'http://127.0.0.1:5000'; }
   public static get apiUploadPictureURL(): string { return this.apiURL + '/pictures/upload'; }
   public static get apiGetAllPicturesURL(): string { return this.apiURL + '/pictures'; }
+  public static get apiDeletePictureURL(): string { return this.apiURL + '/pictures/'; }
 }
